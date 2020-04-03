61cb6f61cca46103f9a4ce24fa5f743cad6377f6
JohnsonShan <43336083+JohnsonShan@users.noreply.github.com>
2020-04-03 15:40:45 +0800

    done trynodejs

---
+++ Practice/TryReact/test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+      // React 的 JSX 程式碼
+      ReactDOM.render(
+        <div class="container"><h1>Hello, world!</h1></div>
+      ,
+      document.getElementById('test')
+    );
