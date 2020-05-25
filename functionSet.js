f85b65e3a77627a911c52b44a8ac1f242c2a4344
JohnsonShan <43336083+JohnsonShan@users.noreply.github.com>
2020-05-25 12:42:32 +0800

    25/5

---
+++ functionSet.js
@@ -0,0 +1,25 @@
+document.querySelector('.my-footer').innerHTML = `
+<p id='footer' class='shadow-sm p-3 mb-5 bg-white rounded '>
+© Copyright  ${new Date().getFullYear()}  Johnson Shan - All Rights Reserved
+</p> 
+`;
+
+if (document.querySelector('.my-nav') != null) {
+    document.querySelector('.my-nav').innerHTML = `
+<ul class='shadow-sm p-3 mb-5 bg-white rounded '>
+<li><a href="index.html">Home</a></li>
+<li><a href="portfolio.html ">Portfolio</a></li>
+<li><a href="blog.html">Blog</a></li>
+</ul>
+`;
+}
+
+if (document.querySelector('.my-nav-blog') != null) {
+    document.querySelector('.my-nav-blog').innerHTML = `
+<ul class='shadow-sm p-3 mb-5 bg-white rounded '>
+<li><a href="../index.html">Home</a></li>
+<li><a href="../portfolio.html ">Portfolio</a></li>
+<li><a href="../blog.html">Blog</a></li>
+</ul>
+`;
+}
