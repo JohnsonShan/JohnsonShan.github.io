d31ecd78896d45998f9c0fd60564074897d5ad9b
Johnson SHAN <johnsonshan@fimmick.com>
2021-04-12 17:26:30 +0800

    cursor

--- animatedBtn.js
+++ animatedBtn.js
@@ -2,14 +2,15 @@ AnimatedBtn = (manifest) => {
     const domId = manifest.domId;
     const transitionTime = manifest.transitionTime;
     const yourFunction = manifest.yourFunction;
-    const leftColor = manifest.leftColor;
-    const rightColor = manifest.rightColor;
+    const coverColor = manifest.coverColor;
+    const baseColor = manifest.baseColor;
     let done;
     let yourInterval;
     let dom = document.querySelector(`${domId}`) || document.querySelector(`#${domId}`);
-    dom.style.background = `linear-gradient(to right, ${leftColor} 50%, ${rightColor} 50%)`;
+    dom.style.background = `linear-gradient(to right, ${coverColor} 50%, ${baseColor} 50%)`;
     dom.style.backgroundSize = '200% 100%';
     dom.style.backgroundPosition = 'right bottom';
+    dom.style.cursor = 'pointer';
     dom.addEventListener('mousedown', (e) => {
         done = false;
         dom.style.transition = `all ${transitionTime / 1000}s` || 'all 1s';
